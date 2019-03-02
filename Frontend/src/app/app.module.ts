e4e6fcc38a2fbe762c2282144a070f7775cb5c2c
Darius Sherman <Darius_Sherman@baylor.edu>
2019-03-02 12:34:26 -0600

    change routing and add login page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
